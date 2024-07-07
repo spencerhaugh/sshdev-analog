a44a907264157b15bd27ae4bb9dee6fd44cfffab
Spencer Haugh <spencer.haugh@gmail.com>
2024-07-07 15:13:11 -0400

    wip

--- src/app/pages/projects/index.page.ts
+++ src/app/pages/projects/index.page.ts
@@ -53,6 +53,15 @@ export default class IndexPage {
         'Material UI', 'Railway']
     },
     {
+      name: 'Spencer Haugh Dot Dev',
+      description: 'This very app!',
+      imgAsset: '/shdev-app.png',
+      link: 'http://www.spencerhaugh.dev',
+      repoLink: '',
+      body: 'This site has been built twice, once in Next JS using React, and once in Analog JS using Angular. I wanted to compare using these two SSR frameworks, and I thought "what better way to learn them than to build the same frontend with each technology?" And so, a project was born.',
+      technologies: ['Next JS', 'Analog JS', 'React', 'Angular', 'SSR', 'Shared backend']
+    },
+    {
       name: 'Hangry',
       description: 'A sassy recipe suggestion app',
       imgAsset: '/hangry-app.png',
