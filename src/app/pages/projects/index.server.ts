693e443bcf88183ee23950493d9dcce788e718cf
Spencer Haugh <spencer.haugh@gmail.com>
2024-07-22 22:31:38 -0400

    projects data fetching

---
+++ src/app/pages/projects/index.server.ts
@@ -0,0 +1,10 @@
+import { PageServerLoad } from '@analogjs/router';
+
+export async function load({ params }: PageServerLoad) {
+  const projectResponse = await fetch(
+    'https://portfolio-server-production-c31f.up.railway.app/projects'
+  );
+  const projectData = await projectResponse.json();
+
+  return { "projectData": projectData };
+}
