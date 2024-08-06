ca099088e495fc80f9cb9d90c577e959ea7e69ba
Spencer Haugh <spencer.haugh@gmail.com>
2024-08-06 17:22:45 -0400

    update link

--- src/refs.ts
+++ src/refs.ts
@@ -1,6 +1,6 @@
 import { NavbarLink } from "./models/nav.model";
 
-export const RESUME_LINK = "https://drive.google.com/file/d/1-AifT8L92THPadpXjZqazPAygQGKalEu/view?usp=sharing";
+export const RESUME_LINK = "https://drive.google.com/file/d/1lGJtliCnaQ1i37dCHmfPrTKDhbGN_pir/view?usp=sharing";
 export const BASE_DATA_URL="https://portfolio-server-production-c31f.up.railway.app";
 
 export const links: NavbarLink[] = [
